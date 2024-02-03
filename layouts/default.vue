ec3c1253e367ef4f6aa4e03b9e43a6f239c64b25
Cristopher Rivas <cristopher_rivas@outlook.es>
2024-02-02 21:00:28 -0600

    default

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-toolbar collapse absolute color="black">
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-toolbar>
+
+  <slot />
+</template>
